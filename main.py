76e4770d27eb68faef4459b20d9432d4bc6a77a1
ytlaptop <yingtaoyt@outlook.com>
2021-04-20 21:20:49 +0800

    bug

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def getData():
     cursor = db.cursor()
     try:
         url = 'http://106.37.208.243:8068/GJZ/Ajax/Publish.ashx'
-        body = 'PageIndex=1&PageSize=10&action=getRealDatas'
+        body = 'PageIndex=1&PageSize=2000&action=getRealDatas'
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
             'ContentLength': str(len(body))
@@ -33,21 +33,25 @@ def getData():
         real_data = [None] * 17
         for t in json_object['tbody']:
             count = 0
-            for tt in t:
-                if '<' in tt and '>' in tt:
-                    real_data[count] = re.findall('>.*?<', tt, re.S)[0][1:-1]
-                else:
-                    real_data[count] = tt
-                    if count == 3:
-                        real_data[count] = str(list(time.localtime())[0]) + '-' + str(real_data[count]) + ':00'
-                count += 1
-            sql = "INSERT INTO cnemc_data VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\'," \
-                  " \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\')" % \
-                  (real_data[0], real_data[1], real_data[2], real_data[3], real_data[4], real_data[5], real_data[6],
-                   real_data[7], real_data[8], real_data[9], real_data[10], real_data[11], real_data[12], real_data[13],
-                   real_data[14], real_data[15], real_data[16])
-            cursor.execute(sql)
-            db.commit()
+            try:
+                for tt in t:
+                    if '<' in tt and '>' in tt:
+                        real_data[count] = re.findall('>.*?<', tt, re.S)[0][1:-1]
+                    else:
+                        real_data[count] = tt
+                        if count == 3:
+                            real_data[count] = str(list(time.localtime())[0]) + '-' + str(real_data[count]) + ':00'
+                    count += 1
+                print(real_data)
+                sql = "INSERT IGNORE INTO cnemc_data VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', " \
+                      "\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\')" % \
+                      (real_data[0], real_data[1], real_data[2], real_data[3], real_data[4], real_data[5], real_data[6],
+                       real_data[7], real_data[8], real_data[9], real_data[10], real_data[11], real_data[12], real_data[13],
+                       real_data[14], real_data[15], real_data[16])
+                cursor.execute(sql)
+                db.commit()
+            except Exception as e:
+                print(e)
 
         sql = 'INSERT INTO log VALUES(\'' + str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))) + \
               '\', \'succeeded\', ' + str(len(json_object['tbody'])) + ')'
@@ -66,6 +70,6 @@ def getData():
 
 # BlockingScheduler
 scheduler = BlockingScheduler()
-# scheduler.add_job(job, 'interval', hours=4)
-scheduler.add_job(getData, 'interval', seconds=5)
+# scheduler.add_job(getData, 'interval', hours=1)
+scheduler.add_job(getData, 'interval', seconds=10)
 scheduler.start()
